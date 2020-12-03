84ba662a0f5a31fa2b6c9e89b6183fc2fcd2bfb5
jessegraham <jesse@fluentinnovations.com>
2020-12-03 10:34:14 -0500

    commit heroku files

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ def main():
         try:
             print('checking GTX 3060')
             check_status()
+            sleep(5)
         except:
             print('error checking GTX3060s', datetime.now().strftime('%m-%d-%y %H:%M'))
         #PS5
         try:
             print('checking PS5')
             ps5_status()
+            sleep(5)
         except:
             print('error checking PS5', datetime.now().strftime('%m-%d-%y %H:%M'))
         #xbox
         try:
             print('checking xbox')
             xbox_status()
-        except:
-            print('error checking Xbox', datetime.now().strftime('%m-%d-%y %H:%M'))
+        except Exception as e:
+            print(e)
+            #print('error checking Xbox', datetime.now().strftime('%m-%d-%y %H:%M'))
         #sleeping loop
-        print('checked all status taking a nap')
-        sleep(600)
+        print('checked all status taking a nap', datetime.now().strftime('%H:%M'))
+        sleep(300)
 
 
 # Press the green button in the gutter to run the script.
